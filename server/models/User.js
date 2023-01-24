898477b8c2c9dfa20c7a773f7e493a6e4529989a
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-24 21:02:10 +1100

    models created/ User model updated

---
+++ server/models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const friendSchema = require('./Friend');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profilePicture: {
+            type: String,
+        },
+        friends: [friendSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;

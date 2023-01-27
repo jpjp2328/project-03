1744909c13329d423b0562bdaff4c3e3323a9d47
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-28 00:56:37 +1100

    commentSchema added

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,2 +1,21 @@
-const { Schema, model } = require('mongoose');
+const { Schema } = require('mongoose');
 
+const commentSchema = new Schema(
+    {
+        text: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+module.exports = commentSchema

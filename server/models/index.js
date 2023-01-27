a705d65b72be28af0359ad7e14377ea8c7f22261
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-28 01:00:36 +1100

    model index.js updated to export all models

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Post = require('./Post');
+const Tag = require('./Tag');
+const Product = require('./Product');
+const Category = require('./Category');
+
+module.exports = { User, Post, Tag, Product, Category };

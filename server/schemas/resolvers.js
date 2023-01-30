96219920db0fc865faf0db62ffc7f510e20b6905
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-30 18:41:10 +1100

    post/posts query resolvers added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,36 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User, Product, Category, Post, Tag } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         user: async (parent, args, context) => {
             if (context.user) {
-                return User.findOne({ _id: context.user._id }).populate(['friends','posts', 'products'])
+                return User.findOne({ _id: context.user._id })
+                .populate(['friends','posts', 'products']);
             }
             throw new AuthenticationError("You need to be logged in!")
         },
-        posts: async (parents, args, context) => {
-            
-        }
+        post: async (parents, args) => {
+            return Post.findOne({ _id: args._id })
+            .populate(['author', 'tags', 'likes', 'comments']);
+        },
+        posts: async (parents, { tag, name }) => {
+            const params = {};
+
+            if (tag) {
+                params.tag = tag;
+            }
+
+            if (name) {
+                params.name = {
+                    $regex: name,
+                };
+            }
+
+            return Post.find(params).populate('tag');
+        },
+        
+
     },
 }

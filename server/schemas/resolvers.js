c0a07d606b33c602afad14fd6fc23ec35d756cc5
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-30 19:01:47 +1100

    all query resolvers added, formatted

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,13 +7,13 @@ const resolvers = {
         user: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({ _id: context.user._id })
-                .populate(['friends','posts', 'products']);
+                    .populate(['friends', 'posts', 'products']);
             }
             throw new AuthenticationError("You need to be logged in!")
         },
         post: async (parents, args) => {
             return Post.findOne({ _id: args._id })
-            .populate(['author', 'tags', 'likes', 'comments']);
+                .populate(['author', 'tags', 'likes', 'comments']);
         },
         posts: async (parents, { tag, name }) => {
             const params = {};
@@ -28,9 +28,38 @@ const resolvers = {
                 };
             }
 
-            return Post.find(params).populate('tag');
+            return Post.find(params)
+                .populate(['author', 'tags', 'likes', 'comments']);
         },
-        
+        product: async (parent, args) => {
+            return Product.findOne({ _id: args._id })
+                .populate(['seller', 'category']);
+        },
+        products: async (parent, { category, name }) => {
+            const params = {};
 
+            if (category) {
+                params.category = category;
+            }
+
+            if (name) {
+                params.name = {
+                    $regex: name,
+                };
+            }
+
+            return Product.find(params)
+                .populate(['seller', 'category']);
+        },
+        tags: async () => Tag.find(),
+        categories: async () => Category.find(),
+        likes: async (parent, args, context) => {
+            const post = await Post.findOne({ _id: args.postId });
+            return post.likes;
+        },
+        comments: async (parent, args, context) => {
+            const post = await Post.findOne({ _id: args.postId });
+            return post.comments;
+        },
     },
 }

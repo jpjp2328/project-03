29ccd350b172b0d70841748015db72cb40584699
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-31 21:25:06 +1100

    queries added

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,42 @@
 import { gql } from '@apollo/client';
 
-export const GET_USER = gql`
-
+export const GET_TAGS = gql`
+  query Query {
+    tags {
+        _id
+        name
+    }
+  }
 `
+
+export const GET_CATEGORIES = gql `
+  query Query {
+    categories {
+        _id
+        name
+    }
+  }
+`
+export const GET_POST = gql`
+`
+export const GET_ALL_POSTS = gql`
+`
+export const GET_PRODUCTS = gql`
+`
+export const GET_ALL_PRODUCTS = gql`
+`
+// need 'me' query in schema
+export const GET_PROFILE = gql`
+`
+// update 'user' query in schema
+export const GET_USER = gql`
+`
+
+//GET_FRIENDS? or can get them via User
+
+//GET_COMMENTS and GET_LIKES? could get data via Post
+
+
+
+
+

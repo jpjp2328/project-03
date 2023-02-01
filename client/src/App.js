68853b8e9088be5afe7e45b39bf27cf02585059c
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-02-01 19:52:36 +1100

    map posts and render as cards, tested

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,24 @@ function App() {
   }).then(result => setPosts(result.data.allPosts));
 
   return (
-    <p>Hello</p>
+    <>
+      <div className='container'>
+        <div className='row p-5'>
+          {posts.map(post => (
+            <div className='col-md-4' key={post.id}>
+              <div className='card'>
+                <div className='card-body'>
+                  <div className='card-title'>
+                    <h4>{post.title}</h4>
+                  </div>
+                  <p className='card-text'>{post.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 

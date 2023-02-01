b603c1f8b69790140fa1dda125e615a5f49e382f
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-02-01 23:34:05 +1100

    Signup form added to signup page

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,39 +24,31 @@ export const ADD_USER = gql`
   }
 `;
 
-export const CREATE_POST = gql`
-`;
+// export const CREATE_POST = gql``;
 
-// export const UPDATE_POST = gql``
+// export const UPDATE_POST = gql``;
 
-export const DELETE_POST = gql`
-`;
+// export const DELETE_POST = gql``;
 
-export const CREATE_PRODUCT = gql`
-`;
-// export const UPDATE_PRODUCT = gql``
+// export const CREATE_PRODUCT = gql``;
 
-export const DELETE_PRODUCT = gql`
-`;
+// export const UPDATE_PRODUCT = gql``;
 
-export const ADD_FRIEND = gql`
-`;
+// export const DELETE_PRODUCT = gql``;
 
-export const REMOVE_FRIEND = gql`
-`;
+// export const ADD_FRIEND = gql``;
 
-export const LIKE_POST = gql`
-`;
+// export const REMOVE_FRIEND = gql``;
 
-export const UNLIKE_POST = gql`
-`;
+// export const LIKE_POST = gql``;
 
-export const CREATE_COMMENT = gql`
-`;
-// export const UPDATE_COMMENT = gql``
+// export const UNLIKE_POST = gql``;
 
-export const DELETE_COMMENT = gql`
-`;
+// export const CREATE_COMMENT = gql``;
+
+// export const UPDATE_COMMENT = gql``;
+
+// export const DELETE_COMMENT = gql``;
 
 
 

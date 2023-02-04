4a6a16ddd73aab44d3d48cde1d2bbc5dc9dbd2dc
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-02-05 05:04:58 +1100

    Cards now showing username, formatted

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const PostCard = ({ post }) => {
-    const { text, image, username } = post
+    const { text, image, author } = post
     return (
         <div className='card text-center' style={{ minHeight: '350px' }}>
             <div className='card-body'>
                 <img src={image.url} key={image.public_id} alt={image.public_id} style={{ height: '100px' }} className='img-thumbnail m-3' />
-                <h4 className='text-primary'>{username}</h4>
+                <h4 className='text-primary'>{author.username}</h4>
                 <p>{text}</p>
             </div>
         </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
         <div className='container'>
             <div className='row p-5'>
                 {data.allPosts.map(post => (
-                    <div className='col-md-4' key={post._id}>
+                    <div className='col-md-4 p-2' key={post._id}>
                         <PostCard post={post}/>
                     </div>
                 ))}

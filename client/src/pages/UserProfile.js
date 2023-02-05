ccd02f11b157635a9af45f1c5074f21f06eef463
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-02-06 01:22:07 +1100

    cards of users posts added to user profile page

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -3,9 +3,10 @@ import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 
 import Auth from '../utils/auth';
-import { GET_SINGLE_USER } from '../utils/queries';
+import { GET_SINGLE_USER, GET_POST_BY_USER } from '../utils/queries';
 import Sidebar from '../components/Sidebar';
 import UserCard from '../components/UserCard';
+import PostCard from '../components/PostCard';
 
 const UserProfile = () => {
     let params = useParams();
@@ -14,7 +15,7 @@ const UserProfile = () => {
         variables: { username: params.username }
     });
 
-    
+    const { data: posts } = useQuery(GET_POST_BY_USER);
 
     if (loading) return <p> Loading... </p>;
 
@@ -29,8 +30,20 @@ const UserProfile = () => {
                             </div>
                             <div className='col-md-10'>
                                 <div className='container justify-content-center' style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <br/>
+                                    <br />
                                     <UserCard user={data.singleUser} />
+                                </div>
+                                <hr />
+                                <div className='p-5 my-5'>
+                                    <h4 className='text-center p-2'>{data.singleUser.username}'s Posts</h4>
+                                    <div className='row p-3'>
+                                        {posts &&
+                                            posts.postByUser.map(post => (
+                                                <div className='col-md-4 p-2' key={post._id}>
+                                                    <PostCard post={post} />
+                                                </div>
+                                            ))}
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -38,7 +51,7 @@ const UserProfile = () => {
                 </>
             ) : (
                 <>
-                Login or SignUp to view user!
+                    Login or SignUp to view user!
                 </>
             )}
         </div>
